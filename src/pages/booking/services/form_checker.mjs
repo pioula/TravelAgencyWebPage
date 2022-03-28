758d69144b77079ea73472d134bfa8ee8c0d0aa0
Piotr Ulanowski <pioula111@gmail.com>
2022-03-28 23:41:11 +0200

    information what is wrong with input

--- src/pages/booking/services/form_checker.mjs
+++ src/pages/booking/services/form_checker.mjs
@@ -1,9 +1,23 @@
+/* eslint-disable no-param-reassign */
 const bookingForm = document.forms.booking_form;
 const { fname, lname } = bookingForm.elements;
 
+function checkIfNameCorrect(input) {
+  return input.value === '' || input.value > 40;
+}
+
+function incorrectInput(input, e) {
+  e.preventDefault();
+  input.style.backgroundColor = 'red';
+  input.style.borderColor = '#853B00';
+  input.parentNode.querySelector('#input_error').style.display = 'block';
+}
+
 bookingForm.addEventListener('submit', (e) => {
-  if (fname.value === '' || lname.value === ''
-      || fname.value.length > 40 || lname.value.length > 40) {
-    e.preventDefault();
+  if (checkIfNameCorrect(fname)) {
+    incorrectInput(fname, e);
+  }
+  if (checkIfNameCorrect(lname)) {
+    incorrectInput(lname, e);
   }
 });
