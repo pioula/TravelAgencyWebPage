6430cd04171b2f0982f6996960149b3d7dca03cf
Piotr Ulanowski <pioula111@gmail.com>
2022-03-28 20:09:03 +0200

    fname and lname cannot be longer than 40

--- src/pages/booking/services/form_checker.mjs
+++ src/pages/booking/services/form_checker.mjs
@@ -2,7 +2,8 @@ const bookingForm = document.forms.booking_form;
 const { fname, lname } = bookingForm.elements;
 
 bookingForm.addEventListener('submit', (e) => {
-  if (fname.value === '' || lname.value === '') {
+  if (fname.value === '' || lname.value === ''
+      || fname.value.length > 40 || lname.value.length > 40) {
     e.preventDefault();
   }
 });
