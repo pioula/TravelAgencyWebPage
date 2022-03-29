c331eb46f3a0138deb6186e0a03232bb72e6d560
Piotr Ulanowski <pioula111@gmail.com>
2022-03-29 14:19:55 +0200

    ok modal

--- src/pages/booking/services/form_checker.mjs
+++ src/pages/booking/services/form_checker.mjs
@@ -2,22 +2,97 @@
 const bookingForm = document.forms.booking_form;
 const { fname, lname } = bookingForm.elements;
 
+const SECOND = 1000;
+const MODAL_CLEAR = 3;
+
 function checkIfNameCorrect(input) {
   return input.value === '' || input.value > 40;
 }
 
-function incorrectInput(input, e) {
-  e.preventDefault();
+function incorrectInput(input) {
   input.style.backgroundColor = 'red';
   input.style.borderColor = '#853B00';
   input.parentNode.querySelector('#input_error').style.display = 'block';
 }
 
+function createModalBackground() {
+  const modalBackground = document.createElement('div');
+  modalBackground.style.display = 'block';
+  modalBackground.style.position = 'fixed';
+  modalBackground.style.zIndex = 1;
+  modalBackground.style.paddingTop = '100px';
+  modalBackground.style.left = 0;
+  modalBackground.style.top = 0;
+  modalBackground.style.width = '100%';
+  modalBackground.style.height = '100%';
+  modalBackground.style.overflow = 'auto';
+  modalBackground.style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
+  return modalBackground;
+}
+
+function createModalSummary() {
+  const modalSummary = document.createElement('ul');
+  Array.from(bookingForm.elements)
+    .filter((e) => e.tagName.toLowerCase() === 'input')
+    .map((e) => e.value)
+    .forEach((e) => {
+      const listElement = document.createElement('li');
+      listElement.innerHTML = e;
+      modalSummary.appendChild(listElement);
+    });
+  return modalSummary;
+}
+
+function createModalContent() {
+  const modalContent = document.createElement('div');
+  modalContent.style.backgroundColor = '#fefefe';
+  modalContent.style.margin = 'auto';
+  modalContent.style.padding = '20px';
+  modalContent.style.border = '1px solid #888';
+  modalContent.style.width = '80%';
+  return modalContent;
+}
+
+function createModalOkButton(closeModalTimeout, clearModalFunction) {
+  const modalOkButton = document.createElement('button');
+  modalOkButton.innerHTML = 'Ok';
+  modalOkButton.addEventListener('click', () => {
+    clearTimeout(closeModalTimeout);
+    clearModalFunction();
+  });
+  return modalOkButton;
+}
+
+function generateSuccessModal() {
+  const modal = createModalBackground();
+  const modalContent = createModalContent();
+  const modalSummary = createModalSummary();
+
+  const clearModalFunction = () => document.body.removeChild(modal);
+  const closeModalTimeout = setTimeout(() => document
+    .body
+    .removeChild(modal), MODAL_CLEAR * SECOND);
+
+  const modalOkButton = createModalOkButton(closeModalTimeout, clearModalFunction);
+
+  modalContent.appendChild(modalSummary);
+  modalContent.appendChild(modalOkButton);
+  modal.appendChild(modalContent);
+  document.body.appendChild(modal);
+}
+
 bookingForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let isCorrect = true;
   if (checkIfNameCorrect(fname)) {
-    incorrectInput(fname, e);
+    incorrectInput(fname);
+    isCorrect = false;
   }
   if (checkIfNameCorrect(lname)) {
-    incorrectInput(lname, e);
+    incorrectInput(lname);
+    isCorrect = false;
+  }
+  if (isCorrect) {
+    generateSuccessModal();
   }
 });
