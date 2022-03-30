d43af52be0ac28bf96a794fa6eef327aa344fc2d
Piotr Ulanowski <pioula111@gmail.com>
2022-03-30 15:24:05 +0200

    focus, single element, signle event

--- src/pages/booking/services/form_focus.mjs
+++ src/pages/booking/services/form_focus.mjs
@@ -1,4 +1,4 @@
-Array.from(document.forms.booking_form.elements)
+/* Array.from(document.forms.booking_form.elements)
   .filter((e) => e.tagName.toLowerCase() === 'input')
   .forEach((input) => {
     input.addEventListener('focusin', (event) => {
@@ -9,4 +9,26 @@ Array.from(document.forms.booking_form.elements)
       // eslint-disable-next-line no-param-reassign
       event.target.style.borderColor = '';
     });
-  });
+  }); */
+
+let previousInput;
+
+// document.forms.booking_form.addEventListener(['focusin', 'focusout'], (event) => {
+//   if (previousInput) {
+//     previousInput.style.borderColor = '';
+//   }
+//   // eslint-disable-next-line no-param-reassign
+//   event.target.style.borderColor = 'red';
+//   previousInput = event.target;
+// });
+
+document.addEventListener('click', (event) => {
+  if (previousInput) {
+    previousInput.style.borderColor = '';
+  }
+  if (event.target.tagName === 'INPUT') {
+    // eslint-disable-next-line no-param-reassign
+    event.target.style.borderColor = 'azure';
+    previousInput = event.target;
+  }
+});
